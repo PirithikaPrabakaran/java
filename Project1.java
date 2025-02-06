ce4bc96d3d7a3d6c65862987f5269cfae1f84670
PirithikaPrabakaran <prithikaa2006@gmail.com>
2025-02-06 12:46:43 +0530

    Create Project1.java

---
+++ Project1.java
@@ -0,0 +1,25 @@
+class Project1  {
+    
+    public static void main (String [] args ){
+        
+        boolean condition = true;
+        int num = 8 ;
+        char variable = 'P';
+        short s = 16 ;
+        byte b = 4;
+        long l = 635467;
+        float f = 2.7645653f;
+        double d = 4.651237;
+        
+        System.out.println("Boolean Condition : "+condition);
+        System.out.println("Integer Value : "+num);
+        System.out.println("Char value : "+variable);
+        System.out.println("Short Value : "+s);
+        System.out.println("IByte Value : "+b);
+        System.out.println("Long Value : "+l);
+        System.out.println("Float Value : "+f);
+        System.out.println("Double Value : "+d);
+        
+    }
+    
+}
